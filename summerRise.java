b3e964e2d237e3c9e1acd943cb4e44cb5068e23d
Naotaka Kinoshita <naotakakinoshita@gmail.com>
2017-01-04 15:30:51 -0500

    splits work

--- summerRise.java
+++ summerRise.java
@@ -3,10 +3,14 @@ import java.io.*;
 
 public class summerRise {
     private static String text = "";
+    private ArrayList<String>paras = new ArrayList<String>();
+    private static String[] para;
 
-    public static void main (String[]args) {
+    public static void paraSplit(String txt) {
+	para = txt.split("\n\n");
+    }
+    public static void loadText(String filename) {
 	try{
-	String filename = args[0];
 	Scanner scan = new Scanner(new File(filename));
 	while (scan.hasNext()) {
 	    text += scan.nextLine() + "\n";
@@ -15,5 +19,11 @@ public class summerRise {
 	    System.out.println("File not found");
 	}
 	System.out.println(text);
+    }
+
+    public static void main (String[]args) {
+	loadText(args[0]);
+	paraSplit(text);
+	System.out.println(Arrays.toString(para));
     }
 }
