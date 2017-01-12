f2a8c8deaf54e3dd4b975aa7396081ea34a5fc91
xchen5 <xchen5@stuy.edu>
2017-01-11 21:02:44 -0500

    new test, ignore rand periods, title points

--- summerRise.java
+++ summerRise.java
@@ -2,190 +2,253 @@ import java.util.*;
 import java.io.*;
 
 public class summerRise {
-    private static String text = "";
-    private static String[] para;
-    private static String[][] sentencePoints;
-    private static String[][]words;
-    private static ArrayList<String>irrelevant = new ArrayList<String>();
+  private static String text = "";
+  private static String[] para;
+  private static String[][] sentencePoints;
+  private static String[][]words;
+  private static ArrayList<String>irrelevant = new ArrayList<String>();
+  private static ArrayList<String>titleWords;
+  private static String title;
 
-    public static void addIrrelevant (){
-	try{
-	    Scanner scan = new Scanner(new File("irrelevant.txt"));
-	    while (scan.hasNext()) {
-		irrelevant.add(scan.nextLine());
-	    }
-	}catch(FileNotFoundException e) {
-	    System.out.println("Irrelevant File not found");
-	}
-
-    }
-    public static void paraSplit(String txt) {
-	para = txt.split("\n\n");
+  public static void addIrrelevant (){
+    try{
+      Scanner scan = new Scanner(new File("irrelevant.txt"));
+      while (scan.hasNext()) {
+        irrelevant.add(scan.nextLine());
+      }
+    }catch(FileNotFoundException e) {
+      System.out.println("Irrelevant File not found");
     }
 
-    public static void sentenceSplit(String txt) {
-	String[]sentencePointsTemp = txt.split("(?<!\\w\\.\\w.)(?<![A-Z][a-z]\\.)(?<=\\.|\\?)(\\s|[A-Z].*)");
-	sentencePoints = new String[sentencePointsTemp.length][2];
-	for (int i = 0; i < sentencePointsTemp.length; i++) {
-	    sentencePoints[i][0] = sentencePointsTemp[i];
-	    sentencePoints[i][1] = "0";
-	}
+  }
+  public static void paraSplit(String txt) {
+    para = txt.split("\n\n");
+  }
 
-    }
-    //Supposed to sort array based on the second dimension
-    /*public static void sortTwoArray(String[][]) {
+  public static String replaceExceptions(String txt) {
+    txt = txt.replace("Mr. ", "Mr` ");
+    txt = txt.replace("Ms. ", "Ms` ");
+    txt = txt.replace("Dr. ", "Dr` ");
+    txt = txt.replace("Mrs. ", "Mrs` ");
+    txt = txt.replace("Mr. ", "Mr` ");
+    txt = txt.replace("Jr. ", "Jr` ");
+    txt = txt.replace("Sr. ", "Sr` ");
+    txt = txt.replace("vs. ", "vs` ");
+    txt = txt.replace("... ", "``` ");
 
-      }*/
-    public static void assignPoints(String txt) {
-	sentenceSplit(txt);
-	sentencePoints[0][1] = "" + Integer.parseInt(sentencePoints[0][1]) + 2;
-	sentencePoints[sentencePoints.length - 1][1] = "" + Integer.parseInt(sentencePoints[sentencePoints.length - 1][1]) + 1;
-	for (int i = 0; i < sentencePoints.length; i ++) {
-	    String tempSentence = sentencePoints[i][0].toLowerCase();
-	    tempSentence = removePunctuation(tempSentence);
-	    //System.out.println(tempSentence);
-	    for (int n = 0; n < words.length; n++) {
-		if (tempSentence.contains(words[n][0])) {
-		    if (Integer.parseInt(words[n][1]) > 1) {
-			sentencePoints[i][1] = 
-			    Integer.toString(Integer.parseInt(sentencePoints[i][1])
-					     + (Integer.parseInt(words[n][1]) - 1));
-		    }
-		}
-	    }
-	}
-	//System.out.println((Arrays.deepToString(sentencePoints)).replace("\n", "\\n"));
-    }
-    public static String removePunctuation(String txt) {
-	txt = txt.replace("\"", "" );
-	txt = txt.replace(",\"", "" );
-	txt = txt.replace(", ", " ");
-	txt = txt.replace(". ", " ");
-	txt = txt.replace(".\n\n", " ");
-	txt = txt.replace(".\n", " ");
-	txt = txt.replace("! ", " ");
-	txt = txt.replace("!\n\n", " ");
-	txt = txt.replace("!\n", " ");
-	txt = txt.replace("? ", " ");
-	txt = txt.replace("?\n\n", " ");
-	txt = txt.replace("?\n", " ");
-	if (txt.endsWith(".\n")) {
-	    txt = txt.substring(0,txt.length() - 2);
-	}
-	else if (txt.endsWith(".")) {
-	    txt = txt.substring(0,txt.length() - 1);
-	}
-	else if (txt.endsWith("?")) {
-	    txt = txt.substring(0,txt.length() - 1);
-	}
-	else if (txt.endsWith("?\n")) {
-	    txt = txt.substring(0,txt.length() - 2);
-	}
-
-	txt = txt.replace("\n", "");
-	return txt;
+    txt = txt.replace(" A. ", " A` ");
+    txt = txt.replace(" B. ", " B` ");
+    txt = txt.replace(" C. ", " C` ");
+    txt = txt.replace(" D. ", " D` ");
+    txt = txt.replace(" E. ", " E` ");
+    txt = txt.replace(" F. ", " F` ");
+    txt = txt.replace(" G. ", " G` ");
+    txt = txt.replace(" H. ", " H` ");
+    txt = txt.replace(" I. ", " I` ");
+    txt = txt.replace(" J. ", " J` ");
+    txt = txt.replace(" K. ", " K` ");
+    txt = txt.replace(" L. ", " L` ");
+    txt = txt.replace(" M. ", " M` ");
+    txt = txt.replace(" N. ", " N` ");
+    txt = txt.replace(" O. ", " O` ");
+    txt = txt.replace(" P. ", " P` ");
+    txt = txt.replace(" Q. ", " Q` ");
+    txt = txt.replace(" R. ", " R` ");
+    txt = txt.replace(" S. ", " S` ");
+    txt = txt.replace(" T. ", " T` ");
+    txt = txt.replace(" U. ", " U` ");
+    txt = txt.replace(" V. ", " V` ");
+    txt = txt.replace(" W. ", " W` ");
+    txt = txt.replace(" X. ", " X` ");
+    txt = txt.replace(" Y. ", " Y` ");
+    txt = txt.replace(" Z. ", " Z` ");
+    return txt;
+  }
+  public static void sentenceSplit(String txt) {
+    txt = replaceExceptions(txt);
+    String[]sentencePointsTemp = txt.split("(?<!\\w\\.\\w.)(?<![A-Z][a-z]\\.)(?<=\\.|\\?)(\\s|[A-Z].*)");
+    sentencePoints = new String[sentencePointsTemp.length][2];
+    for (int i = 0; i < sentencePointsTemp.length; i++) {
+      sentencePoints[i][0] = sentencePointsTemp[i];
+      sentencePoints[i][1] = "0";
     }
 
-    public static void wordCount(String txt) {
-	addIrrelevant();
-	txt = removePunctuation(txt);
-	txt = txt.toLowerCase();
-	int count = 1;
-	String[]temp = txt.split(" ");
-	Arrays.sort(temp);
-	ArrayList<String>tempArrList = new ArrayList<String>();
-	for (int i = 0; i <temp.length; i++){
-	    tempArrList.add(temp[i]);
-	}
-	//System.out.println(Arrays.toString(temp));
-	int tempArrCounter = 0;
-	while (tempArrCounter < tempArrList.size()) {
-	    if (irrelevant.contains(tempArrList.get(tempArrCounter))) {
-		tempArrList.remove(tempArrList.get(tempArrCounter));
-	    }
-	    else {
-		tempArrCounter ++;
-	    }
-	}
-	for (int i = 0; i < tempArrList.size() - 1 ; i ++) {
-	    if (!tempArrList.get(i).equals(tempArrList.get(i +1))) {
-		count += 1;
-	    }
-	}
-	words = new String[count][2];
-	for (int i = 0; i < words.length ; i ++) {
-	    words[i][1] = "1";
-	}
-	int index = 0;
-	for (int i = 0; i < tempArrList.size() - 1; i ++) {
-	    if (index == 0) {
-		words[0][0] = tempArrList.get(0);
-	    }
-	    if (!tempArrList.get(i).equals(tempArrList.get(i+1))) {
-		words[index + 1][0] = tempArrList.get(i + 1);
-		words[index + 1][1] = "1";
-		index ++;
-	    }
-	    else {
-		words[index][1] = "" + (Integer.parseInt(words[index][1]) + 1);
-	    }
-	}
-	//System.out.println(Arrays.deepToString(words));
+  }
+
+  public static void assignPoints(String txt) {
+    sentenceSplit(txt);
+    sentencePoints[0][1] = "" + Integer.parseInt(sentencePoints[0][1]) + 2;
+    sentencePoints[sentencePoints.length - 1][1] = "" + Integer.parseInt(sentencePoints[sentencePoints.length - 1][1]) + 1;
+    for (int i = 0; i < sentencePoints.length; i ++) {
+      String tempSentence = sentencePoints[i][0].toLowerCase();
+      tempSentence = removePunctuation(tempSentence);
+      //System.out.println(tempSentence);
+      for (int y = 0; y <titleWords.size(); y++) {
+        if (tempSentence.contains(titleWords.get(y))) {
+          sentencePoints[i][1] =   Integer.toString(Integer.parseInt(sentencePoints[i][1]) + 2);
+        }
+      }
+      for (int n = 0; n < words.length; n++) {
+        if (tempSentence.contains(words[n][0])) {
+          if (Integer.parseInt(words[n][1]) > 1) {
+            sentencePoints[i][1] =
+            Integer.toString(Integer.parseInt(sentencePoints[i][1])
+            + (Integer.parseInt(words[n][1]) - 1));
+          }
+        }
+      }
+    }
+    System.out.println((Arrays.deepToString(sentencePoints)).replace("\n", "\\n"));
+  }
+  public static String removePunctuation(String txt) {
+    txt = txt.replace("\"", "" );
+    txt = txt.replace(",\"", "" );
+    txt = txt.replace(", ", " ");
+    txt = txt.replace(". ", " ");
+    txt = txt.replace(".\n\n", " ");
+    txt = txt.replace(".\n", " ");
+    txt = txt.replace("! ", " ");
+    txt = txt.replace("!\n\n", " ");
+    txt = txt.replace("!\n", " ");
+    txt = txt.replace("? ", " ");
+    txt = txt.replace("?\n\n", " ");
+    txt = txt.replace("?\n", " ");
+    txt = txt.replace(": ", " ");
+    if (txt.endsWith(".\n")) {
+      txt = txt.substring(0,txt.length() - 2);
+    }
+    else if (txt.endsWith(".")) {
+      txt = txt.substring(0,txt.length() - 1);
+    }
+    else if (txt.endsWith("?")) {
+      txt = txt.substring(0,txt.length() - 1);
+    }
+    else if (txt.endsWith("?\n")) {
+      txt = txt.substring(0,txt.length() - 2);
     }
 
-    public static void loadText(String filename) {
-	try{
-	    Scanner scan = new Scanner(new File(filename));
-	    while (scan.hasNext()) {
-		text += scan.nextLine() + "\n";
-	    }
-	}catch(FileNotFoundException e) {
-	    System.out.println("File not found");
-	}
-    }
+    txt = txt.replace("\n", "");
+    return txt;
+  }
 
-    private static int maxValue(ArrayList<String>ints) {
-	int max = Integer.parseInt(ints.get(0));
-	for (int i = 0; i < ints.size(); i ++) {
-	    if (Integer.parseInt(ints.get(i)) > max) {
-		max = Integer.parseInt(ints.get(i));
-	    }
-	}
-	return max;
-    }
+  public static void wordCount(String txt) {
+    txt = removePunctuation(txt);
+    txt = txt.toLowerCase();
 
-    public static String makeParagraph(String[][]sentences, int num) {
-	String paragraph = "";
-	ArrayList<String>scores = new ArrayList<String>(sentences.length);
-	for (int i = 0 ; i < sentences.length ; i ++ ) {
-	    scores.add(sentences[i][1]);
-	}
-	for (int r = 0 ; r < num ; r ++) {
-	    paragraph += sentences[scores.indexOf("" + maxValue(scores))][0];
-	    scores.set(scores.indexOf("" + maxValue(scores)), "0");
-	    sentences[scores.indexOf("" + maxValue(scores))][1] = "0";
-	}
-        return paragraph;
+    int count = 1;
+    String[]temp = txt.split(" ");
+    Arrays.sort(temp);
+    ArrayList<String>tempArrList = new ArrayList<String>();
+    for (int i = 0; i <temp.length; i++){
+      tempArrList.add(temp[i]);
     }
+    //System.out.println(Arrays.toString(temp));
+    int tempArrCounter = 0;
+    while (tempArrCounter < tempArrList.size()) {
+      if (irrelevant.contains(tempArrList.get(tempArrCounter))) {
+        tempArrList.remove(tempArrList.get(tempArrCounter));
+      }
+      else {
+        tempArrCounter ++;
+      }
+    }
+    for (int i = 0; i < tempArrList.size() - 1 ; i ++) {
+      if (!tempArrList.get(i).equals(tempArrList.get(i +1))) {
+        count += 1;
+      }
+    }
+    words = new String[count][2];
+    for (int i = 0; i < words.length ; i ++) {
+      words[i][1] = "1";
+    }
+    int index = 0;
+    for (int i = 0; i < tempArrList.size() - 1; i ++) {
+      if (index == 0) {
+        words[0][0] = tempArrList.get(0);
+      }
+      if (!tempArrList.get(i).equals(tempArrList.get(i+1))) {
+        words[index + 1][0] = tempArrList.get(i + 1);
+        words[index + 1][1] = "1";
+        index ++;
+      }
+      else {
+        words[index][1] = "" + (Integer.parseInt(words[index][1]) + 1);
+      }
+    }
+    //System.out.println(Arrays.deepToString(words));
+  }
 
-    public static void main (String[]args) {
-	loadText(args[0]);
-	paraSplit(text);
-	wordCount(text);
-	assignPoints(text);
-	try {
-	    if (Integer.parseInt(args[1]) <= sentencePoints.length) {
-		System.out.println(makeParagraph(sentencePoints, 
-						 (Integer.parseInt(args[1]))));
-	    }
-	}catch (NumberFormatException e) {
-	    System.out.println("Format: summerRise.java [filename] [number of sentences]");
-	}catch (ArrayIndexOutOfBoundsException e) {
-	    System.out.println("Format: summerRise.java [filename] [number of sentences]");
-	}
-	/*for (int i = 0; i < irrelevant.size(); i ++) {
-	  System.out.println(irrelevant.get(i));
-	  }*/
-	//System.out.println(Arrays.toString(para));
+  public static void loadText(String filename) {
+    try{
+      addIrrelevant();
+      Scanner scan = new Scanner(new File(filename));
+      String tempTitle = scan.nextLine();
+      title = tempTitle;
+      tempTitle = removePunctuation(tempTitle);
+      tempTitle = tempTitle.toLowerCase();
+      String[]tempTitleArr = tempTitle.split(" ");
+      titleWords = new ArrayList<String>(Arrays.asList(tempTitleArr));
+
+      for (int i = 0; i < titleWords.size(); i++ ) {
+        if (irrelevant.contains(titleWords.get(i))) {
+          titleWords.remove(titleWords.get(i));
+        }
+      }
+      System.out.println(titleWords);
+      System.out.println(title + "\n\n");
+
+      while (scan.hasNext()) {
+        text += scan.nextLine() + "\n";
+      }
+    }catch(FileNotFoundException e) {
+      System.out.println("File not found");
     }
+  }
+
+
+  private static int maxValue(ArrayList<String>ints) {
+    int max = Integer.parseInt(ints.get(0));
+    for (int i = 0; i < ints.size(); i ++) {
+      if (Integer.parseInt(ints.get(i)) > max) {
+        max = Integer.parseInt(ints.get(i));
+      }
+    }
+    return max;
+  }
+
+  public static String makeParagraph(String[][]sentences, int num) {
+    String paragraph = "";
+    ArrayList<String>scores = new ArrayList<String>(sentences.length);
+    for (int i = 0 ; i < sentences.length ; i ++ ) {
+      scores.add(sentences[i][1]);
+    }
+    for (int r = 0 ; r < num ; r ++) {
+      paragraph += sentences[scores.indexOf("" + maxValue(scores))][0];
+      scores.set(scores.indexOf("" + maxValue(scores)), "0");
+      sentences[scores.indexOf("" + maxValue(scores))][1] = "0";
+    }
+    paragraph = paragraph.replace("`", ".");
+    return title + "\n\n" + paragraph;
+  }
+
+  public static void main (String[]args) {
+    loadText(args[0]);
+    paraSplit(text);
+    wordCount(text);
+    assignPoints(text);
+    try {
+      if (Integer.parseInt(args[1]) <= sentencePoints.length) {
+        System.out.println(makeParagraph(sentencePoints,
+        (Integer.parseInt(args[1]))));
+      }
+    }catch (NumberFormatException e) {
+      System.out.println("Format: summerRise.java [filename] [number of sentences]");
+    }catch (ArrayIndexOutOfBoundsException e) {
+      System.out.println("Format: summerRise.java [filename] [number of sentences]");
+    }
+    /*for (int i = 0; i < irrelevant.size(); i ++) {
+    System.out.println(irrelevant.get(i));
+  }*/
+  //System.out.println(Arrays.toString(para));
+}
 }
